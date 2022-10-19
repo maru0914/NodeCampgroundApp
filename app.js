4f69ddeeb58fe081d5f9516126b77bb9edb05504
emmaru <littleoduozu@gmail.com>
2022-10-19 11:26:41 +0900

    add start script

--- app.js
+++ app.js
@@ -169,6 +169,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err });
 });
 
-app.listen(3000, () => {
-    console.log('Server started on port 3000');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
 });
